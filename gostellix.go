372cc69f1db4d3a2736df677778905b27f8570aa
Elliott Sims <elliott@backblaze.com>
2019-11-04 03:46:13 +0000

    Add some more objects to unmarshal to, add JSON tags

--- gostellix.go
+++ gostellix.go
@@ -14,41 +14,80 @@ import (
 const defaultApiURL string = "https://api.dns.constellix.com/"
 const authHeaderName string = "x-cns-security-token"
 
-type constellixSoa struct {
-	PrimaryNameserver string
-	Email string
-	Ttl int
-	Serial int
-	Refresh int
-	Retry int
-	Expire int
-	NegCache int
+type ConstellixSoa struct {
+	PrimaryNameserver string `json:"primaryNameserver,omitempty"`
+	Email string `json:"email,omitempty"`
+	Ttl int `json:"ttl,omitempty"`
+	Serial int `json:"serial,omitempty"`
+	Refresh int `json:"refresh,omitempty"`
+	Retry int `json:"retry,omitempty"`
+	Expire int `json:"expire,omitempty"`
+	NegCache int `json:"negCache,omitempty"`
 }
-type constellixDomain struct {
-	Id int
-	Name string
-	Soa constellixSoa
-	CreatedTs string
-	ModifiedTs string
-	HasGeoIP bool
-	Nameservers []string
-	Status string
+type ConstellixDomain struct {
+	Id int `json:"id,omitempty"`
+	Name string `json:"name"`
+	TypeId int `json:"typeId,omitempty"`
+	HasGtdRegions bool `json:"hasGtdRegions,omitempty"`
+	HasGeoIP bool `json:"hasGeoIP,omitempty"`
+	NameserverGroup int `json:"nameserverGroup,omitempty"`
+	Nameservers []string `json:"nameservers,omitempty"`
+	CreatedTs string `json:"createdTs,omitempty"`
+	ModifiedTs string `json:"modifiedTs,omitempty"`
+	Note string `json:"note,omitempty"`
+	Version int `json:"version,omitempty"`
+	Status string `json:"status,omitempty"`
+	tags []string `json:"tags,omitempty"`
+	Soa ConstellixSoa `json:"soa,omitempty"`
 }
-type constellixRecord struct {
-	Id int
-	Type string
-	RecordType string
-	Name string
-	RecordOption string
-	NoAnswer bool
-	Note string
-	Ttl int
-	GtdRegion int
-	ParentId string
-	Parent string
-	Source string
-	ModifiedTs int
-	value []string
+
+type RoundRobinObj struct {
+	Value string `json:"value"`
+	DisableFlag bool `json:"disableFlag"`
+}
+type GeolocationObj struct {
+	GeoipUserRegion []int `json:"geoipUserRegion,omitempty"`
+	Drop bool `json:"drop,omitifempty"`
+}
+type RecordFailoverValues struct {
+	Value string `json:"value"`
+	CheckId string `json:"checkId"`
+	DisableFlag string `json:"disableFlag"`
+}
+type RecordFailoverObj struct {
+	FailoverType int `json:"failoverType"`
+	Values []RecordFailoverValues `json:"values"`
+	DisableFlag bool `json:"disableFlag"`
+}
+type RoundRobinFailoverObjValues struct {
+	Value string `json:"value"`
+	DisableFlag bool `json:"disableFlag"`
+	CheckId int `json:"checkId"`
+}
+type RoundRobinFailoverObj struct {
+	Values []RoundRobinFailoverObjValues `json:"values"`
+	disableFlag bool `json:"disableFlag"`
+}
+type ConstellixRecord struct {
+	Id int `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	Ttl int `json:"ttl,omitempty"`
+	Geolocation GeolocationObj `json:"geolocation,omitempty"`
+	RecordOption string `json:"recordOption,omitempty"`
+	NoAnswer bool `json:"noAnswer,omitempty"`
+	Note string `json:"note,omitempty"`
+	GtdRegion int `json:"gtdRegion,omitempty"`
+	Type string `json:"type,omitempty"`
+	ParentId int `json:"parentId,omitempty"`
+	Parent string `json:"parent,omitempty"`
+	Source string `json:"source,omitempty"`
+	ContactIds []int `json:"contactIds,omitempty"`
+	RoundRobin []RoundRobinObj `json:"roundRobin"`
+	RecordFailover RecordFailoverObj  `json:"recordFailover,omitifempty"`
+	Pools []int `json:"pools,omitempty"`
+	roundRobinFailover RoundRobinFailoverObj `json:"roundRobinFailover"`
+	ModifiedTs int `json:"modifiedTs,omitempty"`
+	Value []string `json:"value,omitempty"`
 }
 
 type Client struct {
@@ -74,18 +113,38 @@ func NewClient(apikey, secretkey string) *Client {
 	}
 }
 
-func (client *Client) GetAllDomains() ([]constellixDomain, error) {
-	var domains []constellixDomain
+func (client *Client) GetAllDomains() ([]ConstellixDomain, error) {
+	var domains []ConstellixDomain
 	body, _ := client.ApiRequest("v1/domains", "", "GET")
 	json.Unmarshal(body, &domains)
 	return domains, nil
 }
 
-func (client *Client) GetDomain(domainid int) ([]constellixRecord, error) {
-	var records []constellixRecord
+func (client *Client) ListDomains() ([]string, error) {
+	var domainList []string
+	domainObjects, _ := client.GetAllDomains()
+	for _, domain := range domainObjects {
+		domainList = append(domainList, domain.Name)
+	}
+	return domainList, nil
+}
+
+func (client *Client) GetDomain(domainid int) ([]ConstellixRecord, error) {
+	var records []ConstellixRecord
 	body, _ := client.ApiRequest("v1/domains/" + strconv.Itoa(domainid) + "/records", "", "GET")
+	//TODO: go through paging
 	json.Unmarshal(body, &records)
 	return records, nil
+}
+
+func (client *Client) GetRecordsByDomainName(domainName string) ([]ConstellixRecord, error) {
+	allDomains,_ := client.GetAllDomains()
+	for _, domain := range allDomains {
+		if domain.Name == domainName {
+			return client.GetDomain(domain.Id)
+		}
+	}
+	return nil,nil
 }
 
 
