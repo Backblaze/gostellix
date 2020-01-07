b09c4bffe42e61dd081ff66cb40f5a0eea4f9582
Elliott <elliott@elliottlaptop.local>
2020-01-07 00:44:52 -0700

    Fix missing / in URLs, change optional nested structs to pointers so omitifempty will work, add GetDomainByName function

--- gostellix.go
+++ gostellix.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -30,20 +31,20 @@ type ConstellixSoa struct {
 
 // ConstellixDomain holds a domain
 type ConstellixDomain struct {
-	ID              int           `json:"id,omitempty"`
-	Name            string        `json:"name"`
-	TypeID          int           `json:"typeId,omitempty"`
-	HasGtdRegions   bool          `json:"hasGtdRegions,omitempty"`
-	HasGeoIP        bool          `json:"hasGeoIP,omitempty"`
-	NameserverGroup int           `json:"nameserverGroup,omitempty"`
-	Nameservers     []string      `json:"nameservers,omitempty"`
-	CreatedTs       string        `json:"createdTs,omitempty"`
-	ModifiedTs      string        `json:"modifiedTs,omitempty"`
-	Note            string        `json:"note,omitempty"`
-	Version         int           `json:"version,omitempty"`
-	Status          string        `json:"status,omitempty"`
-	Tags            []string      `json:"tags,omitempty"`
-	Soa             ConstellixSoa `json:"soa,omitempty"`
+	ID              int            `json:"id,omitempty"`
+	Name            string         `json:"name"`
+	TypeID          int            `json:"typeId,omitempty"`
+	HasGtdRegions   bool           `json:"hasGtdRegions,omitempty"`
+	HasGeoIP        bool           `json:"hasGeoIP,omitempty"`
+	NameserverGroup int            `json:"nameserverGroup,omitempty"`
+	Nameservers     []string       `json:"nameservers,omitempty"`
+	CreatedTs       string         `json:"createdTs,omitempty"`
+	ModifiedTs      string         `json:"modifiedTs,omitempty"`
+	Note            string         `json:"note,omitempty"`
+	Version         int            `json:"version,omitempty"`
+	Status          string         `json:"status,omitempty"`
+	Tags            []string       `json:"tags,omitempty"`
+	Soa             *ConstellixSoa `json:"soa,omitempty"`
 }
 
 // RoundRobinObj holds RoundRobin records from a domain
@@ -88,25 +89,25 @@ type RoundRobinFailoverObj struct {
 
 // ConstellixRecord domain record
 type ConstellixRecord struct {
-	ID                 int                   `json:"id,omitempty"`
-	Name               string                `json:"name,omitempty"`
-	TTL                int                   `json:"ttl,omitempty"`
-	Geolocation        GeolocationObj        `json:"geolocation,omitempty"`
-	RecordOption       string                `json:"recordOption,omitempty"`
-	NoAnswer           bool                  `json:"noAnswer,omitempty"`
-	Note               string                `json:"note,omitempty"`
-	GtdRegion          int                   `json:"gtdRegion,omitempty"`
-	Type               string                `json:"type,omitempty"`
-	ParentID           int                   `json:"parentId,omitempty"`
-	Parent             string                `json:"parent,omitempty"`
-	Source             string                `json:"source,omitempty"`
-	ContactIDs         []int                 `json:"contactIds,omitempty"`
-	RoundRobin         []RoundRobinObj       `json:"roundRobin"`
-	RecordFailover     RecordFailoverObj     `json:"recordFailover,omitifempty"`
-	Pools              []int                 `json:"pools,omitempty"`
-	RoundRobinFailover RoundRobinFailoverObj `json:"roundRobinFailover"`
-	ModifiedTs         int                   `json:"modifiedTs,omitempty"`
-	Value              []string              `json:"value,omitempty"`
+	ID                 int                    `json:"id,omitempty"`
+	Name               string                 `json:"name,omitempty"`
+	TTL                int                    `json:"ttl,omitempty"`
+	Geolocation        *GeolocationObj        `json:"geolocation,omitempty"`
+	RecordOption       string                 `json:"recordOption,omitempty"`
+	NoAnswer           bool                   `json:"noAnswer,omitempty"`
+	Note               string                 `json:"note,omitempty"`
+	GtdRegion          int                    `json:"gtdRegion,omitempty"`
+	Type               string                 `json:"type,omitempty"`
+	ParentID           int                    `json:"parentId,omitempty"`
+	Parent             string                 `json:"parent,omitempty"`
+	Source             string                 `json:"source,omitempty"`
+	ContactIDs         []int                  `json:"contactIds,omitempty"`
+	RoundRobin         []RoundRobinObj        `json:"roundRobin"`
+	RecordFailover     *RecordFailoverObj     `json:"recordFailover,omitifempty"`
+	Pools              []int                  `json:"pools,omitempty"`
+	RoundRobinFailover *RoundRobinFailoverObj `json:"roundRobinFailover"`
+	ModifiedTs         int                    `json:"modifiedTs,omitempty"`
+	Value              []string               `json:"value,omitempty"`
 }
 
 // Client Client for the Constellix API
@@ -142,6 +143,17 @@ func (client *Client) GetAllDomains() ([]ConstellixDomain, error) {
 	body, _ := client.APIRequest("v1/domains", "", "GET")
 	json.Unmarshal(body, &domains)
 	return domains, nil
+}
+
+// GetDomainByName Get a single domain, by name
+func (client *Client) GetDomainByName(name string) (ConstellixDomain, error) {
+	allDomains, _ := client.GetAllDomains()
+	for domain := range allDomains {
+		if allDomains[domain].Name == name {
+			return allDomains[domain], nil
+		}
+	}
+	return ConstellixDomain{}, errors.New("No such domain")
 }
 
 // ListDomains get a bare list of all domain names
@@ -220,14 +232,14 @@ func (client *Client) DeleteDomainRecord(domainID int, recordType string, record
 // CreateDomainRecord add a record to a domain
 func (client *Client) CreateDomainRecord(domainID int, record ConstellixRecord) error {
 	recordJSON, _ := json.Marshal(record)
-	_, err := client.APIRequest("v1/domains/"+strconv.Itoa(domainID)+"/records"+record.Type, string(recordJSON), "POST")
+	_, err := client.APIRequest("v1/domains/"+strconv.Itoa(domainID)+"/records/"+record.Type, string(recordJSON), "POST")
 	return err
 }
 
 // ModifyDomainRecord add a record to a domain
 func (client *Client) ModifyDomainRecord(domainID int, record ConstellixRecord) error {
 	recordJSON, _ := json.Marshal(record)
-	_, err := client.APIRequest("v1/domains/"+strconv.Itoa(domainID)+"/records"+record.Type, string(recordJSON), "PUT")
+	_, err := client.APIRequest("v1/domains/"+strconv.Itoa(domainID)+"/records/"+record.Type, string(recordJSON), "PUT")
 	return err
 }
 
@@ -253,7 +265,7 @@ func (client *Client) APIRequest(endpoint, params, reqtype string) (response []b
 	if err != nil {
 		return nil, err
 	}
-	//TODO: handle errors.  Network failures, and maybe expired-token failures.
+	//TODO: handle errors.  Network failures, and maybe expired-token failures.  Or, rather, pass them up better
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	return body, err
